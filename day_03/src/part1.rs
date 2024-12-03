e695479cd0d1adbf58d205babecceddc09ab5402
SpizzyCoder <spizzycoder@protonmail.com>
2024-12-03 10:05:00 +0100

    Day 3

---
+++ day_03/src/part1.rs
@@ -0,0 +1,13 @@
+use regex::Regex;
+
+pub fn solve(input: &str) {
+    let mul_regex: Regex = Regex::new(r"mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)").unwrap();
+
+    let mut sum: i32 = 0;
+
+    for mul in mul_regex.captures_iter(input) {
+        sum += mul["num1"].parse::<i32>().unwrap() * mul["num2"].parse::<i32>().unwrap();
+    }
+
+    println!["{}", sum];
+}
---
+++ day_03/src/part2.rs
@@ -0,0 +1,26 @@
+use regex::Regex;
+
+pub fn solve(input: &str) {
+    let mul_regex: Regex =
+        Regex::new(r"(?<statement>(mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\))|do\(\)|don't\(\))")
+            .unwrap();
+
+    let mut sum: i32 = 0;
+    let mut enabled: bool = true;
+
+    for mul in mul_regex.captures_iter(input) {
+        if mul["statement"].starts_with("mul") {
+            if enabled {
+                sum += mul["num1"].parse::<i32>().unwrap() * mul["num2"].parse::<i32>().unwrap();
+            }
+        } else if &mul["statement"] == "do()" {
+            enabled = true;
+        } else if &mul["statement"] == "don't()" {
+            enabled = false;
+        } else {
+            panic![];
+        }
+    }
+
+    println!["{}", sum];
+}
